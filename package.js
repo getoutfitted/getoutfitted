ea333d093576be3d85e98fe522b47cd17d255e40
Paul Grever <paulgrever@Pauls-MacBook-Pro-2.local>
2015-11-09 14:25:45 -0700

    added package info

---
+++ package.js
@@ -0,0 +1,6 @@
+Package.describe({
+  summary: 'Shopify orders are pulled into Reaction',
+  name: 'getoutfitted:reaction-shopify-orders',
+  version: '0.1.0',
+  git: 'https://github.com/getoutfitted/reaction-shopify-orders'
+});
