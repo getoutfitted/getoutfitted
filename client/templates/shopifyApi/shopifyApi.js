9293e0bf64a370bdbcdbaeb79ddbc47efec2cd36
Paul Grever <paulgrever@Pauls-MacBook-Pro-2.local>
2015-11-13 11:30:21 -0700

    refactored from meteor methods to server functions

--- client/templates/shopifyApi/shopifyApi.js
+++ client/templates/shopifyApi/shopifyApi.js
@@ -29,22 +29,7 @@ Template.shopifyApi.events({
         autoHide: true
       });
     }
-    // let numberOfPages = Math.ceil(orderCount / 50);
-    // let pageNumbers = _.range(1, numberOfPages + 1);
-    // let groupId = randomId();
-    // debugger;
     Meteor.call('shopifyOrders/getOrders');
-    // _.each(pageNumbers, function (pageNumber) {
-    //   Meteor.call('shopifyOrders/getOrders', date, pageNumber, function (error, result) {
-    //     if (result) {
-    //       Meteor.call('shopifyOrders/saveQuery', result.data, date, pageNumber, numberOfPages, groupId);
-    //       _.each(result.data.orders, function (order) {
-    //         Meteor.call('shopifyOrders/createReactionOrder', order);
-    //       });
-    //     }
-    //   });
-    // });
-
     Alerts.removeSeen();
     Alerts.add('Your ' + orderCount + ' Shopify Orders have been saved.', 'success', {
       autoHide: true
--- server/methods/apiCalls.js
+++ server/methods/apiCalls.js
@@ -1,10 +1,64 @@
 function formatDateForApi(date) {
   let shopifyOrders = ReactionCore.Collections.Packages.findOne({name: 'reaction-shopify-orders'}).settings.public;
-  // return moment(date).format('YYYY-MM-DD HH:mm');
+
   if (shopifyOrders.lastUpdated) {
-    return moment(date).format('YYYY-MM-DD') + ' 00:00';
+    return moment(date).format('YYYY-MM-DD HH:mm');
+    // return moment(date).format('YYYY-MM-DD') + ' 00:00';
+    // return moment(date).format('2015-11-12') + ' 00:00';
   }
   return moment(new Date('2003-09-20')).format('YYYY-MM-DD');
+}
+
+function createReactionOrder(order) {
+  check(order, Object);
+  let shippingAddress = [ {address: {
+    country: order.shipping_address.country_code,
+    fullName: order.shipping_address.name,
+    address1: order.shipping_address.address1,
+    address2: order.shipping_address.address2,
+    region: order.shipping_address.province_code,
+    postal: order.shipping_address.zip,
+    city: order.shipping_address.city,
+    phone: order.shipping_address.phone
+  }}];
+  let billingAddress = [{address: {
+    country: order.billing_address.country_code,
+    fullName: order.billing_address.name,
+    address1: order.billing_address.address1,
+    address2: order.billing_address.address2,
+    region: order.billing_address.province_code,
+    postal: order.billing_address.zip,
+    city: order.billing_address.city,
+    phone: order.billing_address.phone
+  }}];
+  ReactionCore.Collections.Orders.insert({
+    userId: Random.id(),
+    email: order.email,
+    shopId: ReactionCore.getShopId(),
+    shipping: shippingAddress,
+    billing: billingAddress
+  });
+}
+
+function saveOrdersToShopifyOrders(data, dateTo, pageNumber, pageTotal, groupId) {
+  check(data, Object);
+  check(dateTo, Date);
+  check(pageNumber, Number);
+  check(pageTotal, Number);
+  check(groupId, String);
+  let shopifyOrders = ReactionCore.Collections.Packages.findOne({name: 'reaction-shopify-orders'}).settings;
+  let dateFrom = new Date('2003-09-20'); // This was before Shopify
+  if (shopifyOrders.public) {
+    dateFrom = shopifyOrders.public.lastUpdated;
+  }
+  ReactionCore.Collections.ShopifyOrders.insert({
+    dateFrom: dateFrom,
+    dateTo: dateTo,
+    information: data,
+    pageNumber: pageNumber,
+    pageTotal: pageTotal,
+    groupId: groupId
+  });
 }
 
 
@@ -62,10 +116,10 @@ Meteor.methods({
             page: pageNumber
           }
         }).data;
-        Meteor.call('shopifyOrders/saveOrdersToShopifyOrders', result, date, pageNumber, numberOfPages, groupId);
-        _.each(result.orders, function (order){
-          Meteor.call('shopifyOrders/createReactionOrder', order);
-        })
+        saveOrdersToShopifyOrders(result, date, pageNumber, numberOfPages, groupId);
+        _.each(result.orders, function (order) {
+          createReactionOrder(order);
+        });
       });
     } else {
       _.each(pageNumbers, function (pageNumber) {
@@ -75,62 +129,12 @@ Meteor.methods({
             page: pageNumber
           }
         }).data;
-        Meteor.call('shopifyOrders/saveOrdersToShopifyOrders', result, date, pageNumber, numberOfPages, groupId);
+        saveOrdersToShopifyOrders(result, date, pageNumber, numberOfPages, groupId);
         _.each(result.orders, function (order) {
-          Meteor.call('shopifyOrders/createReactionOrder', order);
+          createReactionOrder(order);
         });
       });
     }
     Meteor.call('shopifyOrders/updateTimeStamp', date);
-  },
-  'shopifyOrders/saveOrdersToShopifyOrders': function (data, dateTo, pageNumber, pageTotal, groupId) {
-    check(data, Object);
-    check(dateTo, Date);
-    check(pageNumber, Number);
-    check(pageTotal, Number);
-    check(groupId, String);
-    let shopifyOrders = ReactionCore.Collections.Packages.findOne({name: 'reaction-shopify-orders'}).settings;
-    let dateFrom = new Date('2003-09-20'); //This was before Shopify
-    if (shopifyOrders.public) {
-      dateFrom = shopifyOrders.public.lastUpdated;
-    }
-    ReactionCore.Collections.ShopifyOrders.insert({
-      dateFrom: dateFrom,
-      dateTo: dateTo,
-      information: data,
-      pageNumber: pageNumber,
-      pageTotal: pageTotal,
-      groupId: groupId
-    });
-  },
-  'shopifyOrders/createReactionOrder': function (order) {
-    check(order, Object);
-    let shippingAddress = [ {address: {
-      country: order.shipping_address.country_code,
-      fullName: order.shipping_address.name,
-      address1: order.shipping_address.address1,
-      address2: order.shipping_address.address2,
-      region: order.shipping_address.province_code,
-      postal: order.shipping_address.zip,
-      city: order.shipping_address.city,
-      phone: order.shipping_address.phone
-    }}];
-    let billingAddress = [{address: {
-      country: order.billing_address.country_code,
-      fullName: order.billing_address.name,
-      address1: order.billing_address.address1,
-      address2: order.billing_address.address2,
-      region: order.billing_address.province_code,
-      postal: order.billing_address.zip,
-      city: order.billing_address.city,
-      phone: order.billing_address.phone
-    }}];
-    ReactionCore.Collections.Orders.insert({
-      userId: Random.id(),
-      email: order.email,
-      shopId: ReactionCore.getShopId(),
-      'shipping': shippingAddress,
-      'billing': billingAddress
-    });
   }
 });
