41e9366e72cbf97ec318f498f986fafc9dddb163
Paul Grever <paulgrever@Pauls-MacBook-Pro-2.local>
2016-01-06 11:46:07 -0700

    updated items to shipped and added a history object in there for orderShipped

--- server/methods/aftership.js
+++ server/methods/aftership.js
@@ -18,6 +18,21 @@ Meteor.methods({
         pastCheckPoints.push(thisCheckPoint);
       });
       const currentShippingStatus = _.last(sortedCheckPoints);
+      let order = ReactionCore.Collections.Orders.findOne({shopifyOrderNumber: shopifyId});
+      if (!order) {
+        throw new Meteor.Error('No Order Found');
+      }
+
+      let items = order.advancedFulfillment.items;
+      _.each(items, function (item) {
+        item.workflow.workflow.push(item.workflow.status);
+        item.workflow.status = 'shipped';
+      });
+      let history = {
+        event: 'orderShipped',
+        userId: 'FedEx',
+        updatedAt: new Date()
+      };
       ReactionCore.Collections.Orders.update({
         shopifyOrderNumber: shopifyId
       }, {
@@ -28,7 +43,11 @@ Meteor.methods({
           'advancedFulfillment.shippingHistory.trackingNumber': msg.tracking_number,
           'advancedFulfillment.shippingHistory.history': pastCheckPoints,
           'advancedFulfillment.shippingHistory.currentCity': currentShippingStatus.city,
-          'advancedFulfillment.shippingHistory.currentState': currentShippingStatus.state
+          'advancedFulfillment.shippingHistory.currentState': currentShippingStatus.state,
+          'advancedFulfillment.items': items
+        },
+        $addToSet: {
+          history: history
         }
       });
     } else {
