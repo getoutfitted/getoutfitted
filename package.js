c8b236d6ba63464b2e5fabac20e3e889a1677ecc
Spencer Norman <spencern@gmail.com>
2016-01-21 00:44:45 -0700

    Bump versions.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: 'Shopify orders are pulled into Reaction',
   name: 'getoutfitted:reaction-shopify-orders',
-  version: '0.3.1',
+  version: '0.4.0',
   git: 'https://github.com/getoutfitted/reaction-shopify-orders'
 });
 
@@ -22,7 +22,7 @@ Package.onUse(function (api) {
   api.use('momentjs:twix@0.7.2');
   api.use('standard-minifiers');
   api.use('dburles:factory@0.3.10');
-  api.use('getoutfitted:reaction-advanced-fulfillment@0.4.0');
+  api.use('getoutfitted:reaction-advanced-fulfillment@0.5.0');
   api.use('getoutfitted:reaction-rental-products@0.1.2');
   api.use('reactioncommerce:reaction-factories');
   api.use('underscorestring:underscore.string@3.2.2');
