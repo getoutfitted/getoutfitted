efaabea69101446667d8fb6343a8bf2d18795b5b
Spencer Norman <spencern@gmail.com>
2016-03-11 15:07:34 -0700

    Product Image viewer stays in variantWidget when scrolling.

--- packages/reaction-product-variant/client/templates/products/productDetail/variants/variantWidget/variantWidget.js
+++ packages/reaction-product-variant/client/templates/products/productDetail/variants/variantWidget/variantWidget.js
@@ -1,5 +1,5 @@
-function stickyPricing() {
-  const bubbleTop = 75;// + "px";
+function stickyWidget() {
+  const bubbleTop = 75; // This must be set identically to the CSS bubble;
   let $variantWidget = $("#variantWidget");
   let $pricingAnchor = $("#description");
   let width = parseInt($variantWidget.outerWidth(), 10) + "px";
@@ -8,18 +8,30 @@ function stickyPricing() {
   function adjustPosition() {
     let anchorTop = $pricingAnchor.offset().top - bubbleTop;
     let scrollTop = $(window).scrollTop();
-    if (scrollTop > anchorTop && $variantWidget.css("position") !== "fixed" || scrollTop > anchorTop && $variantWidget.position().top - bubbleTop < 0) {
+    if (scrollTop > anchorTop && $variantWidget.css("position") !== "fixed" ||
+      (scrollTop > anchorTop && scrollTop > bubbleTop && $variantWidget.css("position") !== "fixed")) {
       $variantWidget.addClass("sticky");
       $variantWidget.css({
         left: left,
         width: width
       });
-    } else if (scrollTop < anchorTop && $variantWidget.css("position") === "fixed") {
-      $variantWidget.removeClass("sticky");
-      $variantWidget.css({
-        left: "",
-        width: ""
+      $("#leadImageContainer").hide(0, function () {
+        $("#variantWidget").prepend($("#leadImageContainer"));
+        $("#leadImageContainer").slideDown(100);
       });
+    } else if ($variantWidget.css("position") === "fixed") {
+      if (scrollTop < 2*bubbleTop ) {
+        console.log(scrollTop, scrollTop < 2*bubbleTop);
+        $variantWidget.removeClass("sticky");
+        $variantWidget.css({
+          left: "",
+          width: ""
+        });
+        $("#leadImageContainer").hide(0, function () {
+          $("#product-content").prepend($("#leadImageContainer"));
+          $("#leadImageContainer").slideDown(50);
+        });
+      }
     }
   }
 
@@ -29,7 +41,7 @@ function stickyPricing() {
 
 Template.variantWidget.onRendered(function () {
   if(window.innerWidth > 767) {
-    stickyPricing();
+    stickyWidget();
   }
 });
 
