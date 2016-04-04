898901313332f89203966bea530f6528155e88a0
Spencer Norman <spencern@gmail.com>
2016-04-04 10:27:34 -0600

    Update product and variant schema to include getoutfitted fields

--- packages/reaction-schemas/common/schemas/products.js
+++ packages/reaction-schemas/common/schemas/products.js
@@ -244,6 +244,44 @@ ReactionCore.Schemas.ProductVariant = new SimpleSchema({
     label: "Updated at",
     type: Date,
     optional: true
+  },
+  // GetOutfitted ProductVariant Schema Additions
+  functionalType: { // functionalType allows us to add-on to the schema for the `simple` and `variant` types
+    type: String,   // and still maintain opportunity to have unique product types.
+    defaultValue: "variant"
+  },
+  location: {
+    label: "Warehouse Storage Location",
+    type: String,
+    optional: true
+  },
+  color: {
+    type: String,
+    optional: true
+  },
+  size: {
+    type: String,
+    optional: true
+  },
+  gender: {
+    type: String,
+    optional: true
+  },
+  pricePerDay: {
+    label: "Daily Rate",
+    type: Number,
+    defaultValue: 0.0,
+    decimal: true,
+    min: 0,
+    optional: true
+  },
+  // rentalPriceBucket: {
+  //   type: [ReactionCore.Schemas.rentalPriceBucket],
+  //   optional: true,
+  // },
+  workflow: { // XXX: Not 100% certain we need this here, definitely need it on inventory and product
+    type: ReactionCore.Schemas.Workflow,
+    optional: true
   }
 });
 
@@ -431,6 +469,33 @@ ReactionCore.Schemas.Product = new SimpleSchema({
   },
   workflow: {
     type: ReactionCore.Schemas.Workflow,
+    optional: true
+  },
+  // GetOutfitted Product Schema Additions
+  functionalType: { // functionalType allows us to add-on to the schema for the `simple` and `variant` types
+    type: String,   // and still maintain opportunity to have unique product types.
+    defaultValue: "simple"
+  },
+  gender: {
+    type: String,
+    optional: true
+  },
+  colors: {
+    type: [String],
+    optional: true
+  },
+  sizes: {
+    type: [String],
+    optional: true
+  },
+  cleaningBuffer: {
+    type: Number,
+    defaultValue: 0,
+    optional: true
+  },
+  productType: { // E.g. product category (Tent, Jacket)
+    type: String,
+    index: 1,
     optional: true
   }
 });
