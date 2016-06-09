3b49625ca61dd6d43e838798c5e43037c3c41c95
Spencer Norman <spencern@gmail.com>
2016-06-09 17:51:32 -0600

    Disallow adding items with disparate price buckets. Fixes #80

--- packages/reaction-product-variant/client/templates/products/productDetail/variants/variantWidget/variantWidget.js
+++ packages/reaction-product-variant/client/templates/products/productDetail/variants/variantWidget/variantWidget.js
@@ -7,13 +7,12 @@ function stickyWidget() {
   let $pricingAnchor = $("#description");
   let width = parseInt($variantWidget.outerWidth(), 10) + "px";
   let left = parseInt($variantWidget.offset().left, 10) + "px";
-  
+
   let verticalOffset = $variantWidget.outerHeight() - 250;
   if (verticalOffset > 115) {
     $variantWidget.css("margin-bottom", `-${verticalOffset}px`);
   }
-  
-  
+
   function adjustPosition() {
     let anchorTop = $pricingAnchor.offset().top - bubbleTop;
     let scrollTop = $(window).scrollTop();
@@ -42,7 +41,7 @@ function stickyWidget() {
 }
 
 Template.variantWidget.onRendered(function () {
-  if(window.innerWidth > 767) {
+  if (window.innerWidth > 767) {
     stickyWidget();
   }
 });
@@ -66,36 +65,68 @@ Template.variantWidget.helpers({
     const current = ReactionProduct.selectedVariant();
     const reservationLength = Session.get("reservationLength");
     const cart = ReactionCore.Collections.Cart.findOne({userId: Meteor.userId()});
+    let defaultReservationLength;
     if (typeof current === "object") {
-      console.log("res length", reservationLength);
       const childVariants = ReactionProduct.getVariants(current._id);
       if (childVariants.length === 0 && current.functionalType === "rentalVariant") {
         let selectedReservation = _.find(current.rentalPriceBuckets, function (priceBucket) {
           return priceBucket.duration === reservationLength + 1;
         });
         if (selectedReservation) {
-          console.log("returning selected reservation", selectedReservation, reservationLength);
+          Session.set("compatibleReservationAvailable", {available: true, reason: "Available"});
           return selectedReservation;
         }
+        // Setup default reservation length from current product default price bucket.
+        if (current.rentalPriceBuckets && current.rentalPriceBuckets[0]) {
+          defaultReservationLength = current.rentalPriceBuckets[0].duration - 1;
+        }
+
         // Determine if product can be added to current cart/reservation.
         if (cart && cart.startTime && cart.endTime) {
           if (cart.items && cart.items.length > 0) {
-            console.log("disallow booking");
-          } else {
-            console.log("reset cart dates to current product");
+            Session.set("compatibleReservationAvailable", {
+              available: false,
+              reason: `<strong>Oh no!</strong><br />
+                      We apologize for the inconvinience! Currently we can't rent camping products and demo products at the same time right now.
+                      <br /><br />Please either finish checking out with your current reservation and come back and book your demo
+                      equipment separately or remove any items currently in your cart to book this item now.`
+            });
+            return {};
           }
+          // reservation length is different than selected dates
+          Session.set("reservationLength", defaultReservationLength);
+          Meteor.call("rentalProducts/setRentalPeriod", cart._id, cart.startTime, moment(cart.startTime).add(defaultReservationLength, "days").toDate());
+          Session.set("compatibleReservationAvailable", {available: true, reason: "Available"});
+          return current.rentalPriceBuckets[0];
         } else if (cart && !cart.startTime && !cart.endTime) {
-          if (current.rentalPriceBuckets && current.rentalPriceBuckets[0]) {
-            Session.set("reservationLength", current.rentalPriceBuckets[0].duration - 1);
+          if (defaultReservationLength) {
+            Session.set("reservationLength", defaultReservationLength);
+            Session.set("compatibleReservationAvailable", {available: true, reason: "Available"});
             return current.rentalPriceBuckets[0];
           }
         }
+
         if (current.rentalPriceBuckets) {
           return current.rentalPriceBuckets[0];
         }
       }
     }
     return {};
+  },
+  availableToBook: function () {
+    const cart = ReactionCore.Collections.Cart.findOne({userId: Meteor.userId()});
+    const compatibleReservationAvailable = Session.get("compatibleReservationAvailable");
+    if (compatibleReservationAvailable) {
+      return compatibleReservationAvailable.available || !cart.items || cart.items && cart.items.length === 0;
+    }
+    return true;
+  },
+  unavailableReason: function () {
+    compatibleReservationAvailable = Session.get("compatibleReservationAvailable");
+    if (compatibleReservationAvailable) {
+      return compatibleReservationAvailable.reason;
+    }
+    return "";
   }
 });
 
@@ -133,25 +164,42 @@ Template.bundleVariantWidget.helpers({
     const cart = ReactionCore.Collections.Cart.findOne({userId: Meteor.userId()});
 
     if (typeof current === "object" && reservationLength) {
-      console.log("res length", reservationLength);
       const childVariants = ReactionProduct.getVariants(current._id);
       if (childVariants.length === 0 && current.functionalType === "bundleVariant") {
         let selectedReservation = _.find(current.rentalPriceBuckets, function (priceBucket) {
           return priceBucket.duration === reservationLength + 1;
         });
         if (selectedReservation) {
+          Session.set("compatibleReservationAvailable", {available: true, reason: "Available"});
           return selectedReservation;
         }
+
+        // Setup default reservation length from current product default price bucket.
+        if (current.rentalPriceBuckets && current.rentalPriceBuckets[0]) {
+          defaultReservationLength = current.rentalPriceBuckets[0].duration - 1;
+        }
+
         // Determine if product can be added to current cart/reservation.
         if (cart && cart.startTime && cart.endTime) {
           if (cart.items && cart.items.length > 0) {
-            console.log("disallow booking");
-          } else {
-            console.log("disallow booking until new dates are selected");
+            Session.set("compatibleReservationAvailable", {
+              available: false,
+              reason: `<strong>Oh no!</strong><br />
+                      We apologize for the inconvinience! Currently we can't rent camping products and demo products at the same time right now.
+                      <br /><br />Please either finish checking out with your current reservation and come back and book your camping
+                      equipment separately or remove any items currently in your cart to book this item now.`
+            });
+            return {};
           }
+          // reservation length is different than selected dates
+          Session.set("reservationLength", defaultReservationLength);
+          Meteor.call("rentalProducts/setRentalPeriod", cart._id, cart.startTime, moment(cart.startTime).add(defaultReservationLength, "days").toDate());
+          Session.set("compatibleReservationAvailable", {available: true, reason: "Available"});
+          return current.rentalPriceBuckets[0];
         } else if (cart && !cart.startTime && !cart.endTime) {
           if (current.rentalPriceBuckets && current.rentalPriceBuckets[0]) {
             Session.set("reservationLength", current.rentalPriceBuckets[0].duration - 1);
+            Session.set("compatibleReservationAvailable", {available: true, reason: "Available"});
             return current.rentalPriceBuckets[0];
           }
         }
@@ -161,6 +209,21 @@ Template.bundleVariantWidget.helpers({
       }
     }
     return {};
+  },
+  availableToBook: function () {
+    const cart = ReactionCore.Collections.Cart.findOne({userId: Meteor.userId()});
+    const compatibleReservationAvailable = Session.get("compatibleReservationAvailable");
+    if (compatibleReservationAvailable) {
+      return compatibleReservationAvailable.available || !cart.items || cart.items && cart.items.length === 0;
+    }
+    return true;
+  },
+  unavailableReason: function () {
+    compatibleReservationAvailable = Session.get("compatibleReservationAvailable");
+    if (compatibleReservationAvailable) {
+      return compatibleReservationAvailable.reason;
+    }
+    return "";
   }
 });
 
@@ -223,13 +286,12 @@ Template.bundleVariantWidget.events({
               if (error) {
                 ReactionCore.Log.error("Failed to add to cart.", error);
                 return error;
-              } else {
-                Meteor.call("productBundler/updateCartItems",
-                      productId,
-                      currentVariant._id,
-                      Session.get("selectedBundleOptions")
-                      );
               }
+              Meteor.call("productBundler/updateCartItems",
+                productId,
+                currentVariant._id,
+                Session.get("selectedBundleOptions")
+              );
             }
           );
         }
@@ -288,7 +350,7 @@ Template.bundleVariantOptions.helpers({
     if (product) {
       return product.productType || product.title;
     }
-    return 'Option';
+    return "Option";
   },
   variantDisplay: function () {
     if (this.label) {
@@ -303,7 +365,6 @@ Template.bundleVariantOptions.helpers({
   hasOptions: function () {
     return this.variantIds.length > 1;
   }
-  
 });
 
 Template.bundleVariantOptions.events({
@@ -318,27 +379,27 @@ Template.bundleVariantOptions.events({
 
 Template.bundleVariantDefaults.helpers({
   defaultOptions: function () {
-    let defaultOptions = _.reduce(this.bundleProducts, function(bundles, bundle) {
-        let id = bundle.variantIds[0].variantId;
-        if (bundle.variantIds.length === 1) {
-          if (bundles[id]) {
-            bundles[id]["quantity"] += 1;
-          } else {
-            bundles[id] = {
-              quantity: 1,
-              title: `${bundle.label}`,
-              option: `${bundle.variantIds[0].label}`
-            }
-          }
+    let defaultOptions = _.reduce(this.bundleProducts, function (bundles, bundle) {
+      let id = bundle.variantIds[0].variantId;
+      if (bundle.variantIds.length === 1) {
+        if (bundles[id]) {
+          bundles[id]["quantity"] += 1;
+        } else {
+          bundles[id] = {
+            quantity: 1,
+            title: `${bundle.label}`,
+            option: `${bundle.variantIds[0].label}`
+          };
         }
-        return bundles;
+      }
+      return bundles;
     }, {});
 
-    return _.map(defaultOptions, function(val) {
+    return _.map(defaultOptions, function (val) {
       return val;
     });
   },
   moreThanOne: function (qty) {
     return qty > 1;
   }
-})
+});
