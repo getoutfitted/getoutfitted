aa74c31a39aeffaf73b9d7c8175a7ce02144a7b6
Spencer Norman <spencern@gmail.com>
2016-06-17 00:00:15 -0600

    Rewrite data-event-action click event to permit unlimited data-event properties

--- packages/reaction-analytics/client/globals.js
+++ packages/reaction-analytics/client/globals.js
@@ -1,27 +1,24 @@
-ReactionAnalytics.track = function (analyticsEvent) {
-  // do nothing if event has no value
-  if (analyticsEvent.value) {
+ReactionAnalytics.track = function (event, properties) {
+  // Must have event to continue
+  if (event) {
+    if (typeof analytics === "object" && analytics.VERSION.length > 0) {
+      analytics.track(event, properties);
+    }
+
+    if (typeof mixpanel === "object" && mixpanel.length > 0) {
+      mixpanel.track(event, properties);
+    }
+
+    const analyticsEvent = Object.assign({action: event, eventType: "event"}, properties);
+
     if (typeof ga === "function") {
       ga("send", "event", analyticsEvent.category, analyticsEvent.action, analyticsEvent.label,
         analyticsEvent.value);
     }
-    if (typeof mixpanel === "object" && mixpanel.length > 0) {
-      mixpanel.track(analyticsEvent.action, {
-        "Category": analyticsEvent.category,
-        "Label": analyticsEvent.label,
-        "Value": analyticsEvent.value
-      });
-    }
-    if (typeof analytics === "object" && analytics.VERSION.length > 0) {
-      analytics.track(analyticsEvent.action, {
-        "Category": analyticsEvent.category,
-        "Label": analyticsEvent.label,
-        "Value": analyticsEvent.value
-      });
-    }
-    console.log("ReactionCore analytics event", analyticsEvent);
+    console.log("analyticsEvent", analyticsEvent);
     return ReactionCore.Collections.AnalyticsEvents.insert(analyticsEvent);
   }
+  return false;
 };
 
 ReactionAnalytics.page = function () {};
--- packages/reaction-analytics/client/startup.js
+++ packages/reaction-analytics/client/startup.js
@@ -1,24 +1,33 @@
 Meteor.startup(function () {
   Tracker.autorun(function () {
-    var coreAnalytics, googleAnalytics, mixpanel, segmentio;
+    let coreAnalytics;
+    let googleAnalytics;
+    let mixpanel;
+    let segmentio;
+
     coreAnalytics = ReactionCore.Collections.Packages.findOne({
       name: "reaction-analytics"
     });
+
     if (!coreAnalytics || !coreAnalytics.enabled) {
       Alerts.removeType("analytics-not-configured");
       return;
     }
-    googleAnalytics = coreAnalytics.settings["public"].googleAnalytics;
-    mixpanel = coreAnalytics.settings["public"].mixpanel;
-    segmentio = coreAnalytics.settings["public"].segmentio;
+
+    segmentio = coreAnalytics.settings.public.segmentio;
+    googleAnalytics = coreAnalytics.settings.public.googleAnalytics;
+    mixpanel = coreAnalytics.settings.public.mixpanel;
+
     if (segmentio.enabled) {
       if (segmentio.api_key) {
-        analytics.load(coreAnalytics.settings["public"].segmentio.api_key);
+        analytics.load(coreAnalytics.settings.public.segmentio.api_key);
         return;
       } else if (!segmentio.api_key && Roles.userIsInRole(Meteor.user(), "admin")) {
         _.defer(function () {
           return Alerts.toast(
-            `Segment Write Key is not configured. <a href="/dashboard/settings/reaction-analytics">Configure now</a> or <a href="/dashboard">disable the Analytics package</a>.`,
+            `Segment Write Key is not configured.
+            <a href="/dashboard/settings/reaction-analytics">Configure now</a>
+            or <a href="/dashboard">disable the Analytics package</a>.`,
             "danger", {
               html: true,
               sticky: true
@@ -28,12 +37,14 @@ Meteor.startup(function () {
     }
     if (googleAnalytics.enabled) {
       if (googleAnalytics.api_key) {
-        ga("create", coreAnalytics.settings["public"].google - analytics.api_key, "auto");
+        ga("create", coreAnalytics.settings.public.googleAnalytics.api_key, "auto");
       } else if (!googleAnalytics.api_key && Roles.userIsInRole(Meteor.user(), "admin")) {
         _.defer(function () {
           return Alerts.toast(
-            `Google Analytics Property is not configured. <a href="/dashboard/settings/reaction-analytics">Configure now</a> or <a href="/dashboard">disable the Analytics package</a>.`,
-            "errorr", {
+            `Google Analytics Property is not configured.
+            <a href="/dashboard/settings/reaction-analytics">Configure now</a>
+            or <a href="/dashboard">disable the Analytics package</a>.`,
+            "error", {
               type: "analytics-not-configured",
               html: true,
               sticky: true
@@ -43,11 +54,13 @@ Meteor.startup(function () {
     }
     if (mixpanel.enabled) {
       if (mixpanel.api_key) {
-        mixpanel.init(coreAnalytics.settings["public"].mixpanel.api_key);
+        mixpanel.init(coreAnalytics.settings.public.mixpanel.api_key);
       } else if (!mixpanel.api_key && Roles.userIsInRole(Meteor.user(), "admin")) {
         _.defer(function () {
           return Alerts.toast(
-            `Mixpanel token is not configured. <a href="/dashboard/settings/reaction-analytics">Configure now</a> or <a href="/dashboard">disable the Analytics package</a>.`,
+            `Mixpanel token is not configured.
+            <a href="/dashboard/settings/reaction-analytics">Configure now</a> or
+            <a href="/dashboard">disable the Analytics package</a>.`,
             "error", {
               type: "analytics-not-configured",
               html: true,
@@ -57,26 +70,37 @@ Meteor.startup(function () {
       }
     }
     if (!Roles.userIsInRole(Meteor.user(), "admin")) {
-      return Alerts.removeType("analytics-not-configured");
+      Alerts.removeType("analytics-not-configured");
     }
   });
-  return $(document.body).click(function (e) {
-    let $targets;
-    $targets = $(e.target).closest("*[data-event-action]");
-    $targets = $targets.parents("*[data-event-action]").add($targets);
-    return $targets.each(function (index, element) {
-      const $element = $(element);
 
-      const analyticsEvent = {
-        eventType: "event",
-        category: $element.data("event-category"),
-        action: $element.data("event-action"),
-        label: $element.data("event-label"),
-        value: $element.data("event-value")
-      };
+  $(document.body).click(function (e) {
+    let $targets = $(e.target).closest("*[data-event-action]");
+    $targets = $targets.parents("*[data-event-action]").add($targets);
+
+    $targets.each(function (index, element) {
+      const $element = $(element);
+      const event = $element.data("event-action");
+      const data = $element.data();
+
+      let properties = Object.keys(data).reduce(
+        function (props, attr) {
+          // if the data value is defined and starts with "event"
+          if (data[attr] && attr.indexOf("event") === 0) {
+            // don't save the event action, we'll pass that separately
+            if (attr !== "eventAction") {
+              // create a new property that is the `data-event`
+              // strip "event" and lowercase the result;
+              const prop = attr.replace("event", "").toLowerCase();
+              // add new property to props object.
+              props[prop] = data[attr];
+            }
+          }
+          return props;
+        }, {});
 
       // call ReactionAnalytics
-      ReactionAnalytics.track(analyticsEvent);
+      ReactionAnalytics.track(event, properties);
     });
   });
 });
