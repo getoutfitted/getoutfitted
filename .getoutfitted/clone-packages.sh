fdb989cc2fa7d72bb5944abaaf876ff13a7e0b68
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-07-07 22:08:01 -0400

    combine clone scripts

--- .getoutfitted/clone-packages.sh
+++ .getoutfitted/clone-packages.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
 
+cd packages
+
 # Clone packages on Circle CI
+
 if [ "$CIRCLE_BRANCH" = "master" ]; then
-  bash ./production-clone.sh
+  git clone -b master git@github.com:getoutfitted/reaction-rental-products.git
+  git clone -b master git@github.com:getoutfitted/reaction-product-importer.git
+  git clone -b master git@github.com:getoutfitted/reaction-advanced-fulfillment.git
+  git clone -b master git@github.com:getoutfitted/reaction-zopim.git
+  git clone -b master git@github.com:getoutfitted/reaction-shipstation.git
+  git clone -b master git@github.com:getoutfitted/static-pages.git
+  git clone -b master git@github.com:getoutfitted/reaction-klaviyo.git
+  git clone -b master git@github.com:getoutfitted/press-feed.git
+  git clone -b master git@github.com:getoutfitted/transit-times.git
+  git clone -b master git@github.com:getoutfitted/slack.git
+  git clone -b master git@github.com:getoutfitted/product-bundler.git
+  git clone -b master git@github.com:getoutfitted/trail-guide.git
 else
-  bash ./development-clone.sh
+  git clone -b development git@github.com:getoutfitted/reaction-rental-products.git
+  git clone -b development git@github.com:getoutfitted/reaction-product-importer.git
+  git clone -b development git@github.com:getoutfitted/reaction-advanced-fulfillment.git
+  git clone -b development git@github.com:getoutfitted/reaction-zopim.git
+  git clone -b development git@github.com:getoutfitted/reaction-shipstation.git
+  git clone -b development git@github.com:getoutfitted/static-pages.git
+  git clone -b development git@github.com:getoutfitted/reaction-klaviyo.git
+  git clone -b development git@github.com:getoutfitted/press-feed.git
+  git clone -b development git@github.com:getoutfitted/transit-times.git
+  git clone -b development git@github.com:getoutfitted/slack.git
+  git clone -b development git@github.com:getoutfitted/product-bundler.git
+  git clone -b development git@github.com:getoutfitted/trail-guide.git
 fi
+
+cd ..
