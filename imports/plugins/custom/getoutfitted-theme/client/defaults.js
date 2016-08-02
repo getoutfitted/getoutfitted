897193d8ebe1db581384f126fdd59cf81da3e9f6
Spencer Norman <spencern@gmail.com>
2016-08-02 16:00:04 -0600

    Fix layout issues

--- imports/plugins/custom/getoutfitted-theme/client/defaults.js
+++ imports/plugins/custom/getoutfitted-theme/client/defaults.js
@@ -3,6 +3,8 @@ import { Session } from "meteor/session";
 DEFAULT_LAYOUT = "getoutfittedLayout";
 // DEFAULT_WORKFLOW = "coreWorkflow";
 
+Session.set("DEFAULT_LAYOUT", "getoutfittedLayout");
+
 Session.set("INDEX_OPTIONS", {
   template: "getoutfittedIndex",
   layoutHeader: "goLayoutHeader",
