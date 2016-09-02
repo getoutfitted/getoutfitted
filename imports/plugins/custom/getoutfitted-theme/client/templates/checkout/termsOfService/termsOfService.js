0de6075420076a869ec0d4345e3ac72b93d963b4
Spencer Norman <spencern@gmail.com>
2016-09-02 15:53:20 -0600

    Track TOS conversion step

--- imports/plugins/custom/getoutfitted-theme/client/templates/checkout/termsOfService/termsOfService.js
+++ imports/plugins/custom/getoutfitted-theme/client/templates/checkout/termsOfService/termsOfService.js
@@ -2,29 +2,22 @@ import { Meteor } from "meteor/meteor";
 import { Template } from "meteor/templating";
 import { Cart } from "/lib/collections";
 
-Template.termsOfService.onRendered(function () {
-  // ReactionAnalytics.trackEventWhenReady("Completed Checkout Step", {
-  //   "step": 4,
-  //   "Step Name": "Select Shipping Option"
-  // });
-  //
-  // ReactionAnalytics.trackEventWhenReady("Viewed Checkout Step", {
-  //   "step": 5,
-  //   "Step Name": "Review Terms of Service"
-  // });
-});
-
 Template.termsOfService.events({
   "click #termsOfService": function (event) {
-    let customerAgreedToTermsOfService = event.target.checked;
+    const customerAgreedToTermsOfService = event.target.checked;
     Meteor.call("cart/customerAgreedToTermsOfService", customerAgreedToTermsOfService);
-    // ReactionAnalytics.trackEventWhenReady("Agree to Terms Of Service");
+    if (typeof analytics === "object") {
+      analytics.track("Completed Checkout Step", {
+        "step": 3,
+        "Step Name": "Agree to Terms of Service"
+      });
+    }
   }
 });
 
 Template.checkoutPayment.helpers({
   customerHasAgreedToTermsOfService: function () {
-    const cart = Cart.findOne({userId: Meteor.userId()})
+    const cart = Cart.findOne({userId: Meteor.userId()});
     if (cart) {
       return cart.customerAgreedToTermsOfService;
     }
