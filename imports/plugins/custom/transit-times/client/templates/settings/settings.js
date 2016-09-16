8a40f0074bace8dd027b018828351812aa47caea
Paul Grever <paulgrever@gmail.com>
2016-09-16 15:34:37 -0600

    added timezone validation to ensure only MST users can enter holidays

--- imports/plugins/custom/transit-times/client/templates/settings/settings.js
+++ imports/plugins/custom/transit-times/client/templates/settings/settings.js
@@ -2,7 +2,8 @@ import { Template } from 'meteor/templating';
 import { Reaction } from '/client/api';
 import { Packages } from '/lib/collections';
 import { TransitTimesPackageConfig } from '../../../lib/collections/schemas';
-
+import moment from 'moment';
+import 'moment-timezone';
 import './settings.html';
 
 Template.transitTimesSettings.helpers({
@@ -14,6 +15,24 @@ Template.transitTimesSettings.helpers({
       name: 'transit-times',
       shopId: Reaction.getShopId()
     });
+  },
+  denverLocal() {
+    return moment.tz.guess() === 'America/Denver';
+  },
+  displayDate() {
+    return moment(this).format('MMMM Do, YYYY');
+  },
+  sortedHolidays() {
+    const pg = Packages.findOne({
+      name: 'transit-times',
+      shopId: Reaction.getShopId()
+    });
+    if (pg && pg.settings && pg.settings.shippingHolidays) {
+      return pg.settings.shippingHolidays.sort(function (date1, date2) {
+        return +date1 - +date2;
+      });
+    }
+    return [];
   }
 });
 
