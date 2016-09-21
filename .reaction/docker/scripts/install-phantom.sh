a67cdafa7abcfcba2cfd7a9a734fe8532d16ae05
Spencer Norman <spencern@gmail.com>
2016-09-21 17:22:06 -0600

    clean up phantom install script

--- .reaction/docker/scripts/install-phantom.sh
+++ .reaction/docker/scripts/install-phantom.sh
@@ -7,10 +7,15 @@ if [ "${INSTALL_PHANTOMJS}" = "true" ]; then
   printf "\n[-] Installing Phantom.js...\n\n"
 
   # Script based off of phantom installation script found here: https://gist.github.com/julionc/7476620
-  apt-get install wget chrpath libssl-dev libxft-dev -y
+  PHANTOM_VERSION="2.1.1"
+  PHANTOM_JS="phantomjs-$PHANTOM_VERSION-linux-x86_64"
+
+  apt-get update
+  apt-get install build-essential wget chrpath libssl-dev libxft-dev -y
 
   cd ~
-  wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+  #wget https://github.com/Medium/phantomjs/releases/download/v2.1.1/$PHANTOM_JS.tar.bz2
+  https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOM_JS.tar.bz2
   tar xvjf $PHANTOM_JS.tar.bz2
 
   mv $PHANTOM_JS.tar.bz2 /usr/local/share/
