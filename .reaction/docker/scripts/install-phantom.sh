2d4aae2f6ce4cc039b417ad97fba26ebeec5161f
Spencer Norman <spencern@gmail.com>
2016-09-21 17:18:40 -0600

    use phantomjs core version instead of Medium version

--- .reaction/docker/scripts/install-phantom.sh
+++ .reaction/docker/scripts/install-phantom.sh
@@ -7,20 +7,10 @@ if [ "${INSTALL_PHANTOMJS}" = "true" ]; then
   printf "\n[-] Installing Phantom.js...\n\n"
 
   # Script based off of phantom installation script found here: https://gist.github.com/julionc/7476620
-  PHANTOM_VERSION="phantomjs-2.1.1"
-  ARCH=$(uname -m)
-
-  if ! [ $ARCH = "x86_64" ]; then
-      $ARCH="i686"
-  fi
-
-  PHANTOM_JS="$PHANTOM_VERSION-linux-$ARCH"
-
-  apt-get update
-  apt-get install build-essential wget chrpath libssl-dev libxft-dev -y
+  apt-get install wget chrpath libssl-dev libxft-dev -y
 
   cd ~
-  wget https://github.com/Medium/phantomjs/releases/download/v2.1.1/$PHANTOM_JS.tar.bz2
+  wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
   tar xvjf $PHANTOM_JS.tar.bz2
 
   mv $PHANTOM_JS.tar.bz2 /usr/local/share/
