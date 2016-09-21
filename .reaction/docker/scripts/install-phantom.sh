6da00747cda637e08f532c1a201fc7156cbcfa51
Spencer Norman <spencern@gmail.com>
2016-09-21 16:54:11 -0600

    Install phantom js without nam

--- .reaction/docker/scripts/install-phantom.sh
+++ .reaction/docker/scripts/install-phantom.sh
@@ -6,8 +6,39 @@ if [ "${INSTALL_PHANTOMJS}" = "true" ]; then
 
   printf "\n[-] Installing Phantom.js...\n\n"
 
-  npm install --silent -g phantomjs-prebuilt
+  # Script from: https://gist.github.com/julionc/7476620
+  # This script install PhantomJS in your Debian/Ubuntu System
+  #
+  # This script must be run as root:
+  # sudo sh install_phantomjs.sh
+  #
 
-  npm cache clear
+  if [[ $EUID -ne 0 ]]; then
+      echo "This script must be run as root" 1>&2
+      exit 1
+  fi
+
+  PHANTOM_VERSION="phantomjs-2.1.1"
+  ARCH=$(uname -m)
+
+  if ! [ $ARCH = "x86_64" ]; then
+      $ARCH="i686"
+  fi
+
+  PHANTOM_JS="$PHANTOM_VERSION-linux-$ARCH"
+
+  sudo apt-get update
+  sudo apt-get install build-essential chrpath libssl-dev libxft-dev -y
+  sudo apt-get install libfreetype6 libfreetype6-dev -y
+  sudo apt-get install libfontconfig1 libfontconfig1-dev -y
+
+  cd ~
+  wget https://github.com/Medium/phantomjs/releases/download/v2.1.1/$PHANTOM_JS.tar.bz2
+  sudo tar xvjf $PHANTOM_JS.tar.bz2
+
+  sudo mv $PHANTOM_JS.tar.bz2 /usr/local/share/
+  sudo ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/share/phantomjs
+  sudo ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin/phantomjs
+  sudo ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/bin/phantomjs
 
 fi
