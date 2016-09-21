886c0e902dc31785deb60db7a834d75c6649745d
Spencer Norman <spencern@gmail.com>
2016-09-21 16:59:13 -0600

    Remove sudo from phantom installation script

--- .reaction/docker/scripts/install-phantom.sh
+++ .reaction/docker/scripts/install-phantom.sh
@@ -27,18 +27,18 @@ if [ "${INSTALL_PHANTOMJS}" = "true" ]; then
 
   PHANTOM_JS="$PHANTOM_VERSION-linux-$ARCH"
 
-  sudo apt-get update
-  sudo apt-get install build-essential chrpath libssl-dev libxft-dev -y
-  sudo apt-get install libfreetype6 libfreetype6-dev -y
-  sudo apt-get install libfontconfig1 libfontconfig1-dev -y
+  apt-get update
+  apt-get install build-essential chrpath libssl-dev libxft-dev -y
+  apt-get install libfreetype6 libfreetype6-dev -y
+  apt-get install libfontconfig1 libfontconfig1-dev -y
 
   cd ~
   wget https://github.com/Medium/phantomjs/releases/download/v2.1.1/$PHANTOM_JS.tar.bz2
-  sudo tar xvjf $PHANTOM_JS.tar.bz2
+  tar xvjf $PHANTOM_JS.tar.bz2
 
-  sudo mv $PHANTOM_JS.tar.bz2 /usr/local/share/
-  sudo ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/share/phantomjs
-  sudo ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin/phantomjs
-  sudo ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/bin/phantomjs
+  mv $PHANTOM_JS.tar.bz2 /usr/local/share/
+  ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/share/phantomjs
+  ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin/phantomjs
+  ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/bin/phantomjs
 
 fi
