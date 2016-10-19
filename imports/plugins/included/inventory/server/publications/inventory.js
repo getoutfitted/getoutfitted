fa2e54ddc32a863c0512f8afc02b9506e595ca4b
Spencer Norman <spencern@gmail.com>
2016-10-19 17:19:52 -0600

    Fixes #238 Accounts and Inventory over publishing 	* Inventory subscription now returns nothing (`this.ready()`) 	* Accounts subscription now returns only the active user even for Admin.

--- imports/plugins/included/inventory/server/publications/inventory.js
+++ imports/plugins/included/inventory/server/publications/inventory.js
@@ -3,18 +3,18 @@ import { Inventory } from "/lib/collections";
 import { Reaction } from "/server/api";
 
 Meteor.publish("Inventory", function () {
-  if (this.userId === null) {
-    return this.ready();
-  }
-  const shopId = Reaction.getShopId();
-  if (!shopId) {
-    return this.ready();
-  }
-  if (Roles.userIsInRole(this.userId, ["admin", "owner", "createProduct"],
-    shopId)) {
-    return Inventory.find({
-      shopId: shopId
-    });
-  }
+  // if (this.userId === null) {
+  //   return this.ready();
+  // }
+  // const shopId = Reaction.getShopId();
+  // if (!shopId) {
+  //   return this.ready();
+  // }
+  // if (Roles.userIsInRole(this.userId, ["admin", "owner", "createProduct"],
+  //   shopId)) {
+  //   return Inventory.find({
+  //     shopId: shopId
+  //   });
+  // }
   return this.ready();
 });
