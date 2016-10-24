9f293f42b91b60c0744391d1fe6776bda2438042
Spencer Norman <spencern@gmail.com>
2016-10-24 12:01:37 -0600

    Remove extraneous payment provider plugins (paypal, braintree, authnet, example)

--- imports/plugins/included/braintree/server/methods/braintree.js
+++
@@ -1,9 +0,0 @@
-import * as BraintreeMethods from "./braintreeMethods";
-import { Meteor } from "meteor/meteor";
-
-Meteor.methods({
-  "braintreeSubmit": BraintreeMethods.paymentSubmit,
-  "braintree/payment/capture": BraintreeMethods.paymentCapture,
-  "braintree/refund/create": BraintreeMethods.createRefund,
-  "braintree/refund/list": BraintreeMethods.listRefunds
-});
