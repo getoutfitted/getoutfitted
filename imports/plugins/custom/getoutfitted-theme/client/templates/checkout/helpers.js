2b9d81a75788c0299a60d4d0d584ab7b2f5b3033
Spencer Norman <spencern@gmail.com>
2016-11-11 18:46:31 -0700

    Don’t show submit button until all checkout elements exist.

--- imports/plugins/custom/getoutfitted-theme/client/templates/checkout/helpers.js
+++ imports/plugins/custom/getoutfitted-theme/client/templates/checkout/helpers.js
@@ -1,5 +1,7 @@
+import { Meteor } from "meteor/meteor";
 import { Template } from "meteor/templating";
-import { Accounts } from "/lib/collections";
+import { Accounts, Cart } from "/lib/collections";
+
 /*
 Template Helpers for checkout templates.
  */
@@ -13,4 +15,23 @@ Template.registerHelper("shippingAddressExists", () => {
     }
   }
   return false;
+});
+
+// Checks to make sure that the shipping and billing addresses exist and that the customer has agreed to our TOS
+// Returns disabled if not ready.
+Template.registerHelper("submitPaymentButtonDisabled", () => {
+  const cart = Cart.findOne({ userId: Meteor.userId() });
+  // check cart existance and that customer has agreed to TOS
+  if (cart && cart.customerAgreedToTermsOfService) {
+    // Check that billing address exists and has postal code
+    if (Array.isArray(cart.billing) && cart.billing[0].address && cart.billing[0].address.postal) {
+      // Check that shipping address exists and has postal code
+      if (Array.isArray(cart.shipping) && cart.shipping[0].address && cart.shipping[0].address.postal) {
+        // Payment button is _not_ disabled
+        return false;
+      }
+    }
+  }
+  // All other conditions, disable payment button
+  return true;
 });
