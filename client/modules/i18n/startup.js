2939c8e25d9d7f2944df3bde578dd3c39c78c798
Spencer Norman <spencern@gmail.com>
2017-01-20 16:41:04 -0700

    Fix i18n startup not finding shop

--- client/modules/i18n/startup.js
+++ client/modules/i18n/startup.js
@@ -40,7 +40,10 @@ Meteor.startup(() => {
   Tracker.autorun(function () {
     if (Reaction.Subscriptions.Shops.ready() && Meteor.user()) {
       const shop = Shops.findOne(Reaction.getShopId());
-      let language = shop.language || "en";
+      let language = "en";
+      if (shop && shop.language) {
+        language = shop.language;
+      }
       if (Meteor.user() && Meteor.user().profile && Meteor.user().profile.lang) {
         language = Meteor.user().profile.lang;
       }
