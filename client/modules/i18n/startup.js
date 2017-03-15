f25b5c77f4522bc645aa904f2ee042bfa003e4dd
Spencer Norman <spencern@gmail.com>
2017-03-15 16:04:19 -0600

    Roll back localStorage shim as it doesn’t appear to have actually fixed anything

--- client/modules/i18n/startup.js
+++ client/modules/i18n/startup.js
@@ -34,20 +34,6 @@ const options = {
 
 
 Meteor.startup(() => {
-  if (typeof localStorage === "object") {
-    try {
-      localStorage.setItem("localStorage", 1);
-      localStorage.removeItem("localStorage");
-    } catch (e) {
-      Storage.prototype._setItem = Storage.prototype.setItem;
-      Storage.prototype.setItem = function () {};
-      console.log(`Your web browser does not support storing settings locally.
-        In Safari, the most common cause of this is using 'Private Browsing Mode'.
-        Some settings may not save or some features may not work properly for you.
-      `);
-    }
-  }
-
   // use tracker autorun to detect language changes
   // this only runs on initial page loaded
   // and when user.profile.lang updates
